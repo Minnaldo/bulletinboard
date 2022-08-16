169affabc371fb13e55e24731639e06fd3e0641a
VillainDev <minnaldo6602@gmail.com>
2022-08-16 10:15:33 +0900

    #1 - data rest 설정
    게시글, 댓글의 json api 를 자동으로
    restful 하게 만들게끔 설정

--- src/main/java/com/min/bulletinboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/min/bulletinboard/repository/ArticleCommentRepository.java
@@ -3,7 +3,9 @@ package com.min.bulletinboard.repository;
 
 import com.min.bulletinboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
--- src/main/java/com/min/bulletinboard/repository/ArticleRepository.java
+++ src/main/java/com/min/bulletinboard/repository/ArticleRepository.java
@@ -2,8 +2,10 @@ package com.min.bulletinboard.repository;
 
 import com.min.bulletinboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // interface 에 @Repository 어노테이션 붙이지 않은 이유 : JpaRepository 에 구현체인 SimpleJpaRepository 에 @Repository 어노테이션이 선언되어 있음. (Command + Alt + B)
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
