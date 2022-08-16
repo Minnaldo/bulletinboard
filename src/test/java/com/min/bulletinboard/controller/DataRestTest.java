41feebc9b0febd08e256044bf352fdd1f5ee488c
VillainDev <minnaldo6602@gmail.com>
2022-08-16 10:18:02 +0900

    #1 - data rest 테스트
    꼭 필요한 테스트는 아니지만, 공부 겸 작성
    data rest 테스트는 인테그레이션 테스트로
    접근할 수 밖에 없다.
    
    DB 접근이 일어나므로 안전을 위해 `@Transactional` 적용
    테스트 안에서는 롤백 정책으로 동작한다.

---
+++ src/test/java/com/min/bulletinboard/controller/DataRestTest.java
@@ -0,0 +1,87 @@
+package com.min.bulletinboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data REST Test")
+@Transactional          // 테스트에서 동작하는 Transactional 에 기본 동작은 Rollback 입니다. 그래서 모든 DataRestTest 밑에 각각의 UnitTest 들은 RollBack 상태로 트랜잭션이 묶이게 된다.
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenReqeustingArticles_thenReturnsArticlesJsonResponse() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void givenNothing_whenReqeustingArticle_thenReturnsArticleJsonResponse() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenReqeustingArticleCommentsFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenReqeustingArticleComments_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void givenNothing_whenReqeustingArticleComment_thenReturnsArticleCommentJsonResponse() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
